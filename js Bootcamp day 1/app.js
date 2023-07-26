7219e576b3d98e5d72297eafa4ae00ed0122a595
Ashish Shukla <akshkgd@gmail.com>
2023-07-26 07:48:49 +0530

    wd

---
+++ js Bootcamp day 1/app.js
@@ -0,0 +1,80 @@
+// basics
+// words and keywords
+// ram morning hdjshdj
+// keyword => anything that js acan understand is a keyword
+// let console for var 
+
+
+
+// variables
+// es5 => var es6 =>  let and const
+// let email = 'ashish@gmail.com';
+// let age = 25;
+// console.log(email, age)
+
+
+
+// data types => to understand what kind of data is stored in my variable
+// strings, numbers, boolean, arrays and objects
+
+let userName = 'ashish';
+let age = 25;
+let isUserActive = true;
+
+// arrays
+// uses of index number => get the value, edit the value and delete the value
+let items = ['bread', 'butter', 'milk'];
+items[0] = 'brown bread'
+
+// console.log(typeof items)
+
+// objects
+// let user = ['ashish', 'ashish@gmail.com', 25, 56000, 9876543210]
+let user = {
+    name: 'ashish',
+    email: 'ashish@gmail.com',
+    age: 25,
+    salary: 56000
+}
+user.address = 'allahabad';
+user.name = 'ashish shukla';
+delete user.salary;
+// console.log(user)
+
+
+
+
+
+// arrays
+// why array data type is object
+// in js arrays are astored as objects
+let arr = [1,2,3,4]
+
+arr['name'] = 'ashihs'
+// console.log(typeof arr)
+
+let list = ['milk', 'bread', 'butter'];
+console.log(list)
+// push to add a new elewment at the end
+// list.push('coffee')
+// pop is used to remove the value from end
+// list.pop()
+// list.pop()
+
+
+// splice(index value, delete count )
+// delete count => how many values to delete
+list.splice(1,0, 'cookies', 'apple')
+console.log(list)
+
+
+
+
+
+
+// objects
+
+
+// pr non
+
+
