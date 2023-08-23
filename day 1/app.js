77c27bdc27ff04f923d9212492c0c7083d5064f2
Ashish Shukla <akshkgd@gmail.com>
2023-08-23 08:05:47 +0530

    wd

---
+++ day 1/app.js
@@ -0,0 +1,101 @@
+// variables
+// data types
+// arrays
+// objects
+
+// console.log('app.js is connected!!')
+
+
+
+// variables 
+// let salt = 'tata salt'
+// let email = 'ashish@gmail.com'
+// console.log(email)
+
+
+// keywords
+// hello, coffee, calender, hfisfbsjfsd
+// let, for, function, uncle??
+
+
+// es5 => var
+// es6 => let and const
+
+// data types
+// string => any value inside single / double quotes
+let name = "ashish";
+// numbers => all numbers without quotes
+let age = '25.5';
+// boolean 
+let isUserOnline = true;
+// objects
+let user = {
+    name: 'ashish', 
+    email: 'aks@gmail.com',
+    salary: 56000, 
+    age: 25,
+    address: 'delhi',
+    mobile: 9876543210
+}
+user.name = 'ashish shukla'
+user.hobby = 'teaching'
+delete user.salary
+
+// console.log(user);
+
+
+
+// array
+let list = ['milk', 'bread', 'butter']
+// index number => add, update, read, delete
+// list[1] = 'brown bread';
+// list[6] = 'apple'
+// push and pop
+// list.push('apple');
+// list.push('coffee');
+// list.pop()
+// list.pop()
+// console.log(list);
+// splice(index, delete count, )
+// list.splice(0, 0, 'apple', 'coffee', 'rice')
+// console.log(list)
+
+
+
+
+
+// array index will always start from 0
+// array index can never be negative
+// array index can never be string
+
+// let arr = ['yash', 'kunal', 'arpit']
+// arr['teacher'] = 'ashish'
+// console.log(arr)
+
+// let demo = {
+//     0: 'yash',
+//     1: 'ashish',
+// }
+// console.log(demo)
+
+
+// primitive data type
+// reference data type => anything which is not
+// reference is primitive => [] {}
+
+
+// let a = [1,2,3]
+// let b = a;
+
+// console.log(b)
+// b.pop()
+
+// console.log(a)
+
+
+
+// primitive
+let a = 5;
+let b = a;
+b = 10;
+console.log(b, a)
