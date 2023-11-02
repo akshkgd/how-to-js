b04b7798187db8212280988a1f040cb1c179bcd8
Ashish Shukla <akshkgd@gmail.com>
2023-11-02 21:41:22 +0530

    wd

---
+++ day 2/app.js
@@ -0,0 +1,112 @@
+// conditionals
+// functions
+// loops
+
+// age > 18 
+// let age = 2;
+
+// if(age> 18){
+//     console.log('eligible to vote')
+// }
+// else{
+//     console.log('not eligible to vote!')
+// }
+
+
+// we can write true or false inside if or something that converts to true or false
+
+
+// truthy and falsy
+
+// if(10){
+//     console.log('i am god!!')
+// }
+
+// truthy => true
+// falsy => false => "" '' 0 -0 undefined and null
+
+
+// let day = 'rainy';
+
+// if(day == 'sunny'){
+//     console.log('wear t shirt')
+// }
+// else if(day == 'cold'){
+//     console.log('wear jacket')
+// }
+// else{
+//     console.log('wear raincoat')
+// }
+
+
+
+// functions
+
+// function grinder(items){
+//     console.log('grinding...', items)
+// }
+
+// grinder('tomatoes');
+// grinder('onions')
+
+
+// function add(a,b){
+//     console.log(a+b)
+// }
+
+
+// add(2,3)
+// add(12,13)
+
+// return
+
+// function sqr(n){
+//     return n*n;
+// }
+
+
+// let result = sqr(3);
+// console.log(sqr(2))
+
+// arrow functions
+
+// functionName=()=>{
+//      logic
+// }
+
+
+cube=(n)=>n*n*n
+
+let result = cube(4);
+// console.log(result)
+
+
+// use of fin in js
+// input from user in js
+// interactive designs js
+
+
+
+// for loop, for each=> on day 5
+// loop => repeat
+
+
+// for loop
+// for(init; condition; increment)
+
+let items = ['milk', 'bread', 'butter', 'icecream']
+for(let i=0; i<4; i++){
+    console.log(items[i])
+}
+
+
+// how to take input from user
+// bill split app
+// bmi app
+// DOM
+
+// 3000
+
+
+// movie app api
+// 
