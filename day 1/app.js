4cc059a80d7d6d960cc8d8b74bfefd7beb9b5e78
Ashish Shukla <akshkgd@gmail.com>
2023-11-24 21:36:04 +0530

    wd

--- day 1/app.js
+++ day 1/app.js
@@ -1,97 +1,84 @@
-// console.log('app.js is connected')
-
-
-// varaibles
-// data types
-// arrays and objects
-// primitive and reference
+// console.log('app.js is connected!!')
 
 
 // variables
-// father, apple, garden, yitoyo, fi ufoisu
-// keyword => something that compiler can understand
+// data types
+// arrays and objects
 
-// var(es5 => depreciated), let and const => es6 
 
-// let email = 'ashish@gmail.com';
-// email = 'yash@gmail.com';
+// variables
 
-// semicolan is js in optional
+// program => console  apple
+// keywords => var, let and const
+
+
+// let email = 'yash@gmail.com';
+// email = 'ashish@gmail.com'
 // console.log(email)
 
+// data types => string, number, boolean, arrays and objects
+// string => anything iside "" ''
+
+// let userName = "yash"
+// console.log(typeof userName);
+
+// let age = 45;
+// console.log(typeof age)
+
+// let isActive = "false";
+// console.log(typeof isActive);
 
 
-// data types => string, numbers(float, negative), boolean, arrays and objects
-let userName = 'yash';
-//string=>  anything inside '' or "" 
-let age = 26.5;
-let userIsOnline = "false";
-// boolean => when we store just true/false that is a boolean value
-
-// arrays and objects
-let items = ['milk', 'bread', 'butter',]
-// index value always starts from zero
-// index values  => read, edit, delete
-items[1] = 'brown bread';
-// push => adds a value at the end of the array
+// [] => creating an array
+let items = ['milk', 'bread', 'butter'];
+// console.log(items)
+// items[1] = 'brown bread';
 // items.push('coffee')
-
-
-// pop to remove the value from the end
-
-// console.log(items);
+// items.push('apple')
 // items.pop()
-// splice => remove values where we want
-// array.splice(index, deleteCount, addNewItems)
-// items.splice(0, 2, 'milk powder')
-// console.log(typeof items)
+// push/pop only works in the last index
+// splice =>(index, deleteCount, addNewValue (optional)) delete the values
+
+// items.splice(1, 1, 'grapes', 'apple', 'coffee')
+// console.log(items)
+
+
+// index => read, edit, update
 
 
 
+// objects
 
-
-
-// let users = ['yash', 'yash@gmail.com', 26, 211003, 45000]
-// let user = {
-//     name: 'yash',
-//     email: 'yash@gmail.com',
-//     age: 26,
-//     zip: 211003,
-//     salary: 45000
-// }
-// user.name = 'yash goel';
+// let user = ['yash', 'allahabad', 24, 8000, 'yash@gmail.com', 'male']
+let user = {
+    name: 'yash',
+    email: 'yash@gmail.com',
+    age: 24,
+    stipend: 8000,
+    gender: 'male',
+    hobbies : ['swimming', 'reading books', 'cooking', 'sleeping']
+}
+// user.name = 'yash goel'
+// delete user.stipend;
 // user.address = 'allahabad'
-// delete user.zip;
-// console.log(typeof user)
+
+
+// user.hobbies[1] = 'teaching'
+// console.log(user.hobbies)
+
+ let users = [
+    {name: 'yash', age: 24, gender: 'male'},
+    {name: 'mayank', age: 14, gender: 'male'},
+    {name: 'neha', age: 32, gender: 'female'},
+    {name: 'arpit', age: 21, gender: 'male'},
+    {name: 'pranjali', age: 29, gender: 'female'},
+    {name: 'bhavna', age: 22, gender: 'female'},
+    {name: 'kunal', age: 20, gender: 'male'},
+    {name: 'navneet', age: 18, gender: 'male'},
+    {name: 'debashish', age:46, gender: 'male'},
+ ]
 
 
 
-// let arr = [1,2,3,4]
-// // index number is always number
-// // index always starts from 0
-// arr[4] = 5;
-// arr['test'] = 'i am an object'
-// console.log(arr)
-
-
-
-
-// winning app
-
-// array of objects
-let users = [
-    {name: 'yash', email:'yash@gmail.com'},
-    {name: 'asish', email:'ashish@gmail.com'},
-    {name: 'neha', email:'neha@gmail.com'},
-    {name: 'kunal', email:'kunal@gmail.com'},
-    {name: 'bhavna', email:'bhavna@gmail.com'},
-    {name: 'raj', email:'raj@gmail.com'},
-    {name: 'mayank', email:'mayank@gmail.com'},
-    {name: 'vipul', email:'vipul@gmail.com'},
-    {name: 'rajat', email:'rajat@gmail.com'},
-    {name: 'saurabh', email:'saurabh@gmail.com'},
-    {name: 'anand', email:'anand@gmail.com'},
-]
-let randomUser = Math.floor(Math.random()*10)
-
-console.log(users[randomUser].name)
+ let random = Math.floor(Math.random()*users.length) 
+ console.log(users[random].name)
